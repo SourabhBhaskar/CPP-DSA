334ed481888d50f8bc34e431cddb8868972035e6
Sourabh Bhaskar <sourabhbhaskar71@gmail.com>
2024-12-02 13:38:11 +0530

    Compiling and executing our program

---
+++ 1_C++/1_GettingStarted/2_CompilingAndExecutingOurProgram.cpp
@@ -0,0 +1,49 @@
+// Compilation Process
+// -------------------
+    // - Preprocessing : Handles directives like #include and #define, before actual compilation starts.
+    // - Compiling     : Converts C++ source code (.cpp file) into an intermediate object file (.o or .obj).
+    // - Linking       : Combines object files and libraries into an executable program.
+
+
+
+
+// Steps to Compile and Execute
+// ----------------------------
+    // - Write the Code: Create a C++ source code file (e.g., program.cpp).
+    // - Compile: Use a C++ compiler (e.g., g++, clang) to compile the program.
+    // - Linking: The compiler links the object files with necessary libraries to generate the executable.
+    // - Run the Program: Execute the generated executable file.
+    // - Example
+        // g++ program.cpp -o program
+        // program.exe
+
+
+
+
+// Common Compilation Errors
+// -------------------------
+    // - Syntax Errors: Missing semicolons, incorrect syntax, etc.
+    // - Linker Errors: Undefined functions or missing libraries.
+
+
+
+
+// Executable
+// -----------
+    // - After successful compilation and linking, an executable file is created (e.g., program.exe).
+    // - This file can be run on the system to perform the program's tasks.
+
+
+
+
+// Using an IDE vs. Command Line
+// -----------------------------
+    // - IDE: Automates the entire process of compiling and running the program (e.g., Visual Studio, Code::Blocks).
+    // - Command Line: Requires manually using commands to compile and execute the program using a compiler like g++.
+
+
+
+
+int main(){
+    return 0;
+}
