248b920b4e954d0f4fd323b405abd075de55831a
Sourabh Bhaskar <sourabhbhaskar71@gmail.com>
2024-12-02 16:57:54 +0530

    Flow of control

---
+++ 1_C++/1_GettingStarted/5_FlowOfControl.cpp
@@ -0,0 +1,20 @@
+// Sequential Execution
+// --------------------
+    // - By default, statements in a program execute sequentially, meaning one after another.
+    // - The first statement executes first, followed by the second, and so on.
+
+
+
+
+// Need for Complex Control
+// -------------------------
+    // - Many programs, including real-world problems like a bookstore application, need more than just sequential execution.
+    // - Programs often require decisions, loops, or alternative paths.
+
+
+
+
+// Flow-of-Control Statement
+// -------------------------
+    // - Programming languages, including C++, provide flow-of-control statements to manage these more complex execution paths.
+    // - These statements help control the order of execution based on conditions or repetition, allowing for branching and loops.
