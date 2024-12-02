1c060e7812c94a66777b955cec312083364df68c
Sourabh Bhaskar <sourabhbhaskar71@gmail.com>
2024-12-02 13:18:36 +0530

    Simple cpp program

---
+++ 1_C++/1_GettingStarted/1_SimpleCppProgram.cpp
@@ -0,0 +1,20 @@
+// C++ main() Function
+// -------------------
+    // - Every C++ program must have one or more functions, with one named main.
+    // - The operating system calls main to run the program.
+    // - main is defined like any other function with a return type (int), a function name (main), optional parameters (empty in this case), and a body.
+
+// Function Definition
+    // - A function has four parts: return type, function name, parameters, and the body.
+    // - The body is enclosed in {} and contains statements, with the return statement marking the end.
+
+// Return Type of main
+    // - main must return an int (an integer).
+    // - A return value of 0 indicates success, and non-zero values indicate errors or other statuses.
+
+// Semicolons
+    // - A semicolon ; is required at the end of most statements in C++. Missing it causes errors.
+
+// Types
+    // - Types define the kind of data a variable holds (e.g., int, float).
+    // - Every variable has a type, which determines the operations that can be performed on it.
